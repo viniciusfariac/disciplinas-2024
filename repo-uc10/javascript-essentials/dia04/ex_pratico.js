be7b973752558b5d08c699e3f53417cb8dd43eaf
Vinicius Faria <vfariacerqueira@gmail.com>
2024-09-16 00:38:22 -0300

    Adicionando javascrpit-essentails dia 04

---
+++ repo-uc10/javascript-essentials/dia04/ex_pratico.js
@@ -0,0 +1,26 @@
+let x = 2;
+let y = 4;
+
+//Operadores aritméticos básicos
+let soma = x + y;
+let subtracao = x - y;
+let multiplicacao = x * y;
+let divisao = x / y;
+let modulo = x % y;
+let exponencial = x ** y;
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo, exponencial)
+
+//Operadores de incrimento e decremento
+console.log(++x); // Incrementa antes de exibir
+console.log(--y); // Decrementa antes de exibir
+
+//Operadores de atribuição composta
+let numero = 5
+let somatri = numero += 10;
+let subtri = numero -= 10;
+let multri = numero*=10;
+let divitri = numero /=10;
+let modulotri = numero %= 10;
+let exponencialtri = numero **= 10;
+console.log(somatri, subtri, multri, divitri, modulotri, exponencialtri)
